563c69b79c288f66aae7d08a35900d0a7456671d
Thalyson Luiz <thalysonluiz@gmail.com>
2022-12-30 12:09:10 -0300

    fUsuarioIem component, formApp

--- App.js
+++ App.js
@@ -11,9 +11,16 @@ import {
   extendTheme,
   VStack,
   Box,
+  FormControl,
+  WarningOutlineIcon,
+  Input,
+  Pressable,
+  FlatList,
+  Avatar,
+  Spacer,
 } from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
+import { UsuarioItem } from "./components/UsuarioItem";
 
 // Define the config
 const config = {
@@ -24,6 +31,38 @@ const config = {
 // extend the theme
 export const theme = extendTheme({ config });
 
+const data = [{
+  id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+  fullName: "Aafreen Khan",
+  timeStamp: "12:47 PM",
+  recentText: "Good Day!",
+  avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+}, {
+  id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+  fullName: "Sujitha Mathur",
+  timeStamp: "11:11 PM",
+  recentText: "Cheer up, there!",
+  avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU"
+}, {
+  id: "58694a0f-3da1-471f-bd96-145571e29d72",
+  fullName: "Anci Barroco",
+  timeStamp: "6:22 PM",
+  recentText: "Good Day!",
+  avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg"
+}, {
+  id: "68694a0f-3da1-431f-bd56-142371e29d72",
+  fullName: "Aniket Kumar",
+  timeStamp: "8:56 PM",
+  recentText: "All the best",
+  avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
+}, {
+  id: "28694a0f-3da1-471f-bd96-142456e29d72",
+  fullName: "Kiara",
+  timeStamp: "12:47 PM",
+  recentText: "I will call today.",
+  avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
+}];
+
 export default function App() {
   return (
     <NativeBaseProvider>
@@ -33,10 +72,47 @@ export default function App() {
         px={4}
         flex={1}
       >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
+        <VStack space={3} alignItems="center">
+          <Heading size="lg">Cadastro Usuários</Heading>
+          <FormControl w="90%" maxW="300px">
+            <FormControl.Label>Email</FormControl.Label>
+            <Input
+              variant="rounded"
+              placeholder="Email"
+              borderColor="coolGray.600"
+            />
+          </FormControl>
+          <FormControl w="90%" maxW="300px">
+            <FormControl.Label>Senha</FormControl.Label>
+            <Input
+              type="password"
+              variant="rounded"
+              placeholder="Senha"
+              borderColor="coolGray.600"
+            />
+          </FormControl>
+          <Pressable
+            onPress={() => console.log("I'm Pressed")}
+            rounded="8"
+            overflow="hidden"
+            borderWidth="1"
+            borderColor="darkBlue.300"
+            maxW="96"
+            shadow="3"
+            bg="darkBlue.700"
+            p="2"
+            _hover={{ bg: "darkBlue.600" }}
+          >
+            <Text
+              color="#FFF"
+            >
+              Cadastrar
+            </Text>
+          </Pressable>
+          <FlatList data={data} renderItem={({
+            item
+          }) => <UsuarioItem item={item} />} keyExtractor={item => item.id} />
+          {/* <HStack space={2} alignItems="center">
             <Text>Edit</Text>
             <Box
               _web={{
@@ -59,7 +135,7 @@ export default function App() {
               Learn NativeBase
             </Text>
           </Link>
-          <ToggleDarkMode />
+          <ToggleDarkMode /> */}
         </VStack>
       </Center>
     </NativeBaseProvider>
